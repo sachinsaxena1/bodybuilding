2bb44d9ea27b08560b5b7a199f04ff81a6626fae
sachin saxena <sachin saxena>
2017-02-11 19:15:05 +0530

    initial draft of project

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# bodybuilding
-bodybuilding project
+# fitnesshub
+fitnesshub project
---
+++ core/src/main/java/com/fitnesshub/core/controller/TestController.java
@@ -0,0 +1,8 @@
+package com.fitnesshub.core.controller;
+
+public class TestController {
+
+	public String getName(){
+		return "Test";
+	}
+}
